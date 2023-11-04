2ec0cfd754a88e18d1b603ac57d242bd0c8366fb
BigFatDuck1 <clementho999@gmail.com>
2023-11-04 12:10:45 +0800

    Created gameboard and associated functions in an object

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+
+//Make gameboard
+let makeGameboard = {
+    gameboard: [],
+
+    height: 3,
+    width: 3,
+
+    makeBoard: function() {
+        //Make rows
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                //Make columns
+                this.gameboard.push([i,j]); //Coordinates
+            }            
+        }
+        return this.gameboard;
+    },
+    
+};
+
+makeGameboard.makeBoard();
+console.log(makeGameboard.gameboard);
