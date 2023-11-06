ce5050e7ae22a8a97fd88d596b526a84cbe092d4
BigFatDuck1 <clementho999@gmail.com>
2023-11-06 11:11:34 +0800

    Changed coordinates to blank slot and magic square ID

--- script.js
+++ script.js
@@ -14,25 +14,35 @@ let gameboardModule = (function (){
     function makeBoard() {
         for (let h = 0; h < height; h++) {
             for (let w = 0; w < width; w++) {
-                gameboard.push([w, h]); //Coordinates
+                //gameboard.push([w, h]); //Coordinates
+                gameboard.push([""]); //Empty string to fill with move later (X or O)
             }
         }
+        return gameboard;
             
     }
 
-    //Just console logs the gameboard, so you can see everything in the console
-    function logBoard() {
-        console.log(gameboard)
+    function appendThirdOrderSquare(board) {
+        let thirdOrderSquareArray = [4,9,2,3,5,7,8,1,6];
+        for (let i = 0; i < board.length; i++) {
+            board[i].push(thirdOrderSquareArray[i]);
+        }
+        return board;
     }
 
-    makeBoard();
+    function logBoard() {
+        console.log(gameboard);
+    }
 
-    return {gameboard, makeBoard, logBoard}
+
+    return {gameboard, makeBoard, logBoard, appendThirdOrderSquare, setDimensions}
     
 })();
 
 //Using IIFE module here is good because gameboardModule is the only variable in the global namespace
 //height and width are private properties but can be changed with setDimensions(h,w)
+gameboardModule.makeBoard();
+gameboardModule.appendThirdOrderSquare(gameboardModule.gameboard);
 gameboardModule.logBoard();
 
 //Player factory
