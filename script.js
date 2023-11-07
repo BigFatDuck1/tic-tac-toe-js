60e4b86f4f4e6ee9e04fae262649c2230c780166
BigFatDuck1 <clementho999@gmail.com>
2023-11-07 11:51:01 +0800

    Working on oneRound()

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 
-//Make gameboard
+//Gameboard related functions (create, update, checkWin)
 let gameboardModule = (function() {
-
-    let gameboard = [];
 
     let height = 3;
     let width = 3;
@@ -13,6 +11,7 @@ let gameboardModule = (function() {
     }
 
     function makeBoard() {
+        let gameboard = [];
         for (let h = 0; h < height; h++) {
             for (let w = 0; w < width; w++) {
                 //gameboard.push([w, h]); //Coordinates
@@ -30,35 +29,29 @@ let gameboardModule = (function() {
         return board;
     }
     
-    //
-    function updateBoard(square, move) {
+    
+    function updateBoard(square, move, board) {
 
-        this.gameboard[square][0] = move;
-
-        console.log(this.gameboard);
+        board[square][0] = move;
         
-        return {gameboard};
+        return {board};
     }
 
-    function logBoard() {
-        console.log(gameboard);
+    function logBoard(board) {
+        console.log(board);
     }
 
-    function checkWin(side) { //Check for X or O?
+    function checkWin(side, board) { //Check for X or O?
         //Whenever a move has been made, call this function
         //Collects all non-empty squares into a new array
         let filled_squares = [];
         let i = 0;
         let purge_these = [];
 
-        this.gameboard.forEach((element) => {
+        board.forEach((element) => {
 
             if (element[0] == side) {
                 filled_squares.push(element);
-                //TODO: clear this after done debugging
-                console.log(filled_squares);
-                console.log(i);
-
                 purge_these.push(i);
             }
             i++;
@@ -66,7 +59,7 @@ let gameboardModule = (function() {
 
         //If there are less than three squares, return false
         if (filled_squares.length < 3) {
-            return false;
+            return {result: false, purge_these};
         }
 
         //If there are three squares, check if they are in a row
@@ -84,29 +77,23 @@ let gameboardModule = (function() {
         }
     }
 
-    function purgeIndices(array) {
+    //Set moves that have been done to =0, effectively clearing that square
+    function purgeIndices(array, board) {
         for (let element of array) {
-            this.gameboard[element][1] = 0;
-            //TODO: clear after debug
-            console.log(this.gameboard);
+            board[element][1] = 0;
         }
     }
 
-    return {gameboard, makeBoard, logBoard, appendThirdOrderSquare, setDimensions, updateBoard, checkWin, purgeIndices}
+    return {
+            makeBoard, 
+            logBoard, 
+            appendThirdOrderSquare, 
+            setDimensions, 
+            updateBoard, 
+            checkWin, 
+            purgeIndices,}
     
 })();
-
-//Using IIFE module here is good because gameboardModule is the only variable in the global namespace
-//height and width are private properties but can be changed with setDimensions(h,w)
-gameboardModule.makeBoard();
-gameboardModule.appendThirdOrderSquare(gameboardModule.gameboard);
-gameboardModule.logBoard();
-//!Test
-gameboardModule.updateBoard(1, "X");
-gameboardModule.updateBoard(5, "X");
-gameboardModule.updateBoard(7, "X");
-let result = gameboardModule.checkWin("X"); //Lose
-gameboardModule.purgeIndices(result.purge_these);
 
 //Player factory
 function createPlayer(name, order) {
@@ -120,7 +107,7 @@ function createPlayer(name, order) {
 //Game order
 const gameFlow = (function () {
 
-    function whoseTurn(current_player) {
+    function nextTurn(current_player) {
         //gameFlow.whoseTurn returns the opposite of the current player
         return 3 - current_player;
     }
@@ -131,6 +118,8 @@ const gameFlow = (function () {
         return {move, current_player};
     }
 
+    return {nextTurn, promptPlayer}
+
 })();
 
 //Trying out the game in console
@@ -138,3 +127,51 @@ const gameFlow = (function () {
 //1. Create players
 let player1 = createPlayer("Player 1", 1);
 let player2 = createPlayer("Player 2", 2);
+
+//2. Game start initialization 
+let gameboard;
+(function gameStart() {
+    //Using IIFE module here is good because gameboardModule is the only variable in the global namespace
+    //height and width are private properties but can be changed with setDimensions(h,w)
+    gameboard = gameboardModule.makeBoard();
+    gameboardModule.appendThirdOrderSquare(gameboard);
+    //gameboardModule.logBoard(); 
+})();
+
+
+//3. Function for one round with checking for win condition
+let state = "Neutral" //Win, Neutral, Draw
+let current_player = 1; //Start with Player 1
+
+
+function oneRound() {
+    let move = gameFlow.promptPlayer(current_player); //Input must be in the form of a "coordinate" for a square i.e. 0-8
+    let side = current_player == 1 ? "O" : "X";
+    gameboardModule.updateBoard(move.move, side, gameboard);
+    let result = gameboardModule.checkWin(side, gameboard);
+    if (result === "false") {
+        gameboardModule.purgeIndices(result.purge_these);
+        current_player = gameFlow.nextTurn(current_player);
+        state = "Neutral";
+        oneRound();
+
+    }
+    else {
+        // console.log(`Player ${current_player} wins`);
+        state = "Win"
+        return state;
+    }
+}
+
+state = oneRound();
+
+
+//!Test (debug)
+// gameboardModule.updateBoard(1, "X");
+// gameboardModule.updateBoard(5, "X");
+// gameboardModule.updateBoard(7, "X");
+// let result = gameboardModule.checkWin("X"); => lose
+//Only purge if lose
+// if (result == "false") {
+//     gameboardModule.purgeIndices(result.purge_these);
+// }
